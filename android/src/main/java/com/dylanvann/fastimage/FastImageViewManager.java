c7a3fba11ae20602a6728c5809f4b5b538ab9494
Shwet Solanki <shwet@dream11.com>
2019-10-21 13:16:21 +0530

    refactor(glide): removes memory cache

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -103,6 +103,7 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl> imple
                     //    - android.resource://
                     //    - data:image/png;base64
                     .load(imageSource.getSourceForLoad())
+                    .skipMemoryCache(true)
                     .apply(FastImageViewConverter.getOptions(source))
                     .apply(RequestOptions.bitmapTransform(new FastImageGradientTransformation(view.getGradient(), view.getGlideUrl().toString())))
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
